e21536a482620d5392e6f2d0f63ef4421dffeae4
Branden Ehrenreich <branden.a.ehrenreich@gmail.com>
2025-01-01 21:49:37 -0500

    basic test for identifier, bump commit for docs, identifier

--- tests/common.py
+++ tests/common.py
@@ -1,12 +1,24 @@
 import pathlib
 
+from neomodel import config
 import pika
 
-# connection configuration
+# rabbitmq connection configuration
 rmq_host = '127.0.0.1'
 rmq_port = 5672
 rmq_user = 'rabbitmq'
 rmq_password = 'rabbitmq'
+
+# neo4j connection configuration
+neo_host = '127.0.0.1'
+neo_port = 7687
+neo_user = 'neo4j'
+neo_password = 'tXOCq81bn7QfGTMJMrkQqP4J1'
+neo_db_name = 'machina'
+
+def db_set_config() -> None:
+    """set database configiration for neomodel"""
+    config.DATABASE_URL = f"bolt://{neo_user}:{neo_password}@{neo_host}:{neo_port}/{neo_db_name}"
 
 _test_dir = pathlib.Path(__file__).resolve().parent
 test_data_dir = pathlib.Path(_test_dir, 'data').resolve()
--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -1,12 +1,22 @@
+import base64
+import hashlib
 import json
+import logging
+import pathlib
+import time
 import unittest
 
-from tests.common import get_rmq_conn, test_data_dir
+from machina.core.models import JPEG
+from tests.common import db_set_config, get_rmq_conn, test_data_dir
 
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 class TestIdentifier(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
+        db_set_config()
         cls.conn = get_rmq_conn()
         cls.channel = cls.conn.channel()
 
@@ -16,7 +26,41 @@ class TestIdentifier(unittest.TestCase):
 
     def test_provided_type(self):
         """test that type is set when provided manually by user"""
-        pass
+        MAX_RETRIES = 5
+
+        png_file = pathlib.Path(test_data_dir, 'test_identifier', 'google.png').resolve()
+
+        with open(png_file, 'rb') as f:
+            logger.info(f'Submitting {png_file}')
+            png_data = f.read()
+            png_md5 = hashlib.md5(png_data).hexdigest()
+            data_encoded = base64.b64encode(png_data).decode()
+
+            data = json.dumps({
+                'data': data_encoded,
+                'type': 'jpeg'          # assert JPEG instead of PNG
+            })
+
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='Identifier',
+                body=data
+            )
+
+            # wait for JPEG node to be created, and validate it
+            for _ in range(MAX_RETRIES):
+                new_jpeg_node = JPEG.nodes.get_or_none(md5=png_md5)
+                if not new_jpeg_node:
+                    time.sleep(2)
+                    logger.info(f"Checking for JPEG Node with md5={png_md5} in db...")
+                    continue
+                else:
+                    logger.info(f"Found md5={png_md5}")
+                    break
+
+            self.assertIsNotNone(new_jpeg_node)
+
+            # TODO use venv to test this out
 
     def test_resolve_via_detailed_type(self):
         """test that type is set using detailed type magic"""
